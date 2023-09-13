63155533459977736f8b7a6b6b1bda5e6f9f4160
Tong Su <90353234+Sue-Tong@users.noreply.github.com>
2023-09-13 12:54:21 -0400

    Update lang.py

--- examples/pytorch/translation/lang.py
+++ examples/pytorch/translation/lang.py
@@ -48,6 +48,7 @@ from transformers import (
     Seq2SeqTrainingArguments,
     default_data_collator,
     set_seed,
+    MultiLingAdapterArguments
 )
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version
@@ -265,7 +266,7 @@ def main():
     # We now keep distinct sets of args, for a cleaner separation of concerns.
 
     parser = HfArgumentParser(
-        (ModelArguments, DataTrainingArguments, Seq2SeqTrainingArguments)
+        (ModelArguments, DataTrainingArguments, Seq2SeqTrainingArguments,MultiLingAdapterArguments,)
     )
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
